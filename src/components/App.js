a466c77c0791d87aef69b0d13f82967e72d24732
Aaditya785 <aadityasandil5@gmail.com>
2023-02-15 13:26:07 +0530

    Signup/Register part complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { useAuth } from '../hooks';
-import { Home, Login } from '../pages';
+import { Home, Login, Signup } from '../pages';
 import { Loader, Navbar } from './';
 
 const Page404 = () => {
@@ -23,6 +23,8 @@ function App() {
           <Route path='/' element={<Home />} />
 
           <Route path='/login' element={<Login />} />
+
+          <Route path='/register' element={<Signup />} />
 
           <Route path="*" element={<Page404 />} />
         </Routes>
